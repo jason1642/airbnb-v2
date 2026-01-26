c44d8b30c2cea3458ee65df4bffa4826bd19d4ea
jason1642 <jason1642@gmail.com>
2026-01-26 17:08:23 -0500

    nest js users

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
+
+console.log(process.env.MONGODB_USERNAME);
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@portfolio-website.halgu.mongodb.net/sample_airbnb?retryWrites=true&w=majority
+      `),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
