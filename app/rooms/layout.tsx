43f13246b2f86b14ddeba213b6d5fef809be2d44
jason1642 <jason1642@gmail.com>
2026-02-10 19:20:27 -0500

    router

--- app/rooms/layout.tsx
+++ app/rooms/layout.tsx
@@ -1,7 +1,14 @@
+import {useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 export default function Layout({children}: {children: React.ReactNode}) {
+    const router = useRouter();
+    useEffect(() => {
+        console.log(router.query.id)
+    }, [])
     return (
         <section>
             {children}
         </section>
     );
-}
+} 
